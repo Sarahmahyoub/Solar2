4b554d6ff32961477d252c53ad7b3d466d48fa35
Sarahmahyoub <sarahmahyoub@gmail.com>
2023-07-10 01:46:49 -0400

    Both code are now in main added manually

--- src/Planet2.java
+++ src/Planet2.java
@@ -1,6 +1,7 @@
 public class Planet2 {
 
-    public static void main(String[] args) {
-        System.out.println("sarah");
-    }
+    String name;
+    String color;
+    String size;
+    String temp;
 }
